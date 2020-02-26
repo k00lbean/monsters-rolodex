95b97d44da42f886ad27bcc99aa1c4ee525a29a6
k00lbeanAZF <k00lbeanAZF@hotmail.com>
2020-02-26 13:33:34 -0500

    Updates

---
+++ precache-manifest.4b7fe1670d7806cd14bbe52528ebf895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a5c132da429bf6b85906f114edcb26",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "720e03682dc52b4045a4",
+    "url": "/monsters-rolodex/static/css/main.f874a08a.chunk.css"
+  },
+  {
+    "revision": "42fc208c438a823bf6f1",
+    "url": "/monsters-rolodex/static/js/2.98a08c22.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.98a08c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "720e03682dc52b4045a4",
+    "url": "/monsters-rolodex/static/js/main.e9db49c5.chunk.js"
+  },
+  {
+    "revision": "3ce411994159d536241c",
+    "url": "/monsters-rolodex/static/js/runtime-main.eddd01e4.js"
+  }
+]);
